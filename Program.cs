f541ee3e8345d640dfdedf1f29143653371f0836
Paulo Campos <146894063+PauloCamposj77@users.noreply.github.com>
2024-01-06 20:55:04 -0300

    DesafioPoo

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesafioPOO.Models;
+
+Smartphone nokia = new Nokia("123456", "Modelo1", "1111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Smartphone iphone =  new Iphone("123456", "Modelo2", "222222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
